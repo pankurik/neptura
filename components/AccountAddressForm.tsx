14a610aeeed4f5c24dc3fbf50a4c8b4b40c6446c
pankurik <pankurikhare17@gmail.com>
2026-06-21 00:43:38 -0700

    Show loading pulse on address save submit.

--- components/AccountAddressForm.tsx
+++ components/AccountAddressForm.tsx
@@ -277,7 +277,7 @@ export default function AccountAddressForm({
       </label>
 
       <div className="flex flex-col gap-3 pt-2 sm:flex-row">
-        <button type="submit" disabled={isSubmitting} className="btn-light-primary flex-1 disabled:opacity-60">
+        <button type="submit" disabled={isSubmitting} className={`btn-light-primary flex-1 disabled:opacity-60 ${isSubmitting ? "account-action-btn--loading" : ""}`}>
           {isSubmitting ? "Saving…" : submitLabel}
         </button>
         <button
